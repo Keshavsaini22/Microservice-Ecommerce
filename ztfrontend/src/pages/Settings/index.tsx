f1caa9d97c4d23f1d5b7c67b39692ac829e26c2d
Keshavsaini22 <keshavsainikesu@gmail.com>
2024-03-21 18:55:55 +0530

    settings done

--- ztfrontend/src/pages/Settings/index.tsx
+++ ztfrontend/src/pages/Settings/index.tsx
@@ -4,80 +4,16 @@ import SettingsHeader from './SettingsHeader'
 import SettingsLeft from './SettingsLeft'
 import TypographyText from '../../components/TypographyText'
 import CustomButton from '../../components/CustomButton'
+import PersonalInfo from './PersonalInfo'
 
 function Settings() {
     return (
-        <Stack m={2}>
+        <Stack p={4} sx={{ backgroundColor: 'rgb(246, 248, 255)' }} height={'100%'}>
             <SettingsHeader />
-            <Stack direction={'row'}>
+            <Stack direction={'row'} sx={{ backgroundColor: 'white', borderRadius: '15px' }} marginTop={4}>
                 <SettingsLeft />
                 <Divider orientation='vertical' />
-                <Stack>
-                    <Stack>
-                        <TypographyText text={'General Information'} fontSize={''} fontWeight={''} />
-                        <TypographyText text={'This is my information'} fontSize={''} fontWeight={''} />
-                    </Stack>
-                    <Divider />
-                    <TypographyText text={'Profile Picture'} fontSize={''} fontWeight={''} />
-                    <Stack direction='row' alignItems={'flex-end'} gap={3}>
-                        <Stack direction='row' alignItems={'center'} gap={1}>
-                            <Avatar sx={{ width: '60px', height: '60px' }} />
-                            <Stack>
-                                <TypographyText text={'Keshav Saini'} fontSize={''} fontWeight={''} />
-                                <TypographyText text={'Role/Title'} fontSize={''} fontWeight={''} />
-                                <TypographyText text={'Location'} fontSize={''} fontWeight={''} />
-                            </Stack>
-                        </Stack>
-                        <CustomButton sxprops={{ sx: { width: '100px', height: '37px' } }} variant={'contained'} text={'Change'} />
-                        <CustomButton sxprops={{ sx: { width: '100px', height: '37px' } }} variant={'outlined'} text={'Delete'} />
-                    </Stack>
-                    <Stack direction={'row'} gap={1} flexWrap={'wrap'}>
-                        <Box>
-                            <Box>
-                                <TypographyText text={'UserName'} fontSize={''} fontWeight={''} />
-                                <TextField id="outlined-basic" variant="outlined" />
-                            </Box>
-                            <Box>
-                                <TypographyText text={'UserName'} fontSize={''} fontWeight={''} />
-                                <TextField id="outlined-basic" variant="outlined" />
-                            </Box>
-                            <Box>
-                                <TypographyText text={'UserName'} fontSize={''} fontWeight={''} />
-                                <TextField id="outlined-basic" variant="outlined" />
-                            </Box>
-                            <Box>
-                                <TypographyText text={'UserName'} fontSize={''} fontWeight={''} />
-                                <TextField id="outlined-basic" variant="outlined" />
-                            </Box>
-                            <Box>
-                                <TypographyText text={'UserName'} fontSize={''} fontWeight={''} />
-                                <TextField id="outlined-basic" variant="outlined" />
-                            </Box>
-                        </Box>
-                        <Box>
-                            <Box>
-                                <TypographyText text={'UserName'} fontSize={''} fontWeight={''} />
-                                <TextField id="outlined-basic" variant="outlined" />
-                            </Box>
-                            <Box>
-                                <TypographyText text={'UserName'} fontSize={''} fontWeight={''} />
-                                <TextField id="outlined-basic" variant="outlined" />
-                            </Box>
-                            <Box>
-                                <TypographyText text={'UserName'} fontSize={''} fontWeight={''} />
-                                <TextField id="outlined-basic" variant="outlined" />
-                            </Box>
-                            <Box>
-                                <TypographyText text={'UserName'} fontSize={''} fontWeight={''} />
-                                <TextField id="outlined-basic" variant="outlined" />
-                            </Box>
-                            <Box>
-                                <TypographyText text={'UserName'} fontSize={''} fontWeight={''} />
-                                <TextField id="outlined-basic" variant="outlined" />
-                            </Box>
-                        </Box>
-                    </Stack>
-                </Stack>
+                <PersonalInfo />
             </Stack>
         </Stack>
     )
