9fc4ea651fa36dc0f12056680c280a6a7cb409dd
Keshavsaini22 <keshavsainikesu@gmail.com>
2024-03-26 18:43:02 +0530

    fffda

--- ztfrontend/src/pages/OrderDetails/OrderShip/index.tsx
+++ ztfrontend/src/pages/OrderDetails/OrderShip/index.tsx
@@ -3,9 +3,12 @@ import React from 'react'
 import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
 import CustomButton from '../../../components/CustomButton';
 import LocalPrintshopOutlinedIcon from '@mui/icons-material/LocalPrintshopOutlined';
+import PersonOutlinedIcon from '@mui/icons-material/PersonOutlined';
+import ShoppingBagOutlinedIcon from '@mui/icons-material/ShoppingBagOutlined';
+import OrderInfoCard from '../../../components/OrderInfoCard';
 function OrderShip() {
     return (
-        <Stack p={2} sx={{ backgroundColor: 'white', borderRadius: '15px' }}>
+        <Stack gap={1} p={2} sx={{ backgroundColor: 'white', borderRadius: '15px' }}>
             <Stack direction={'row'} gap={3}>
                 <Box sx={{ fontSize: '18px', fontWeight: '700' }}>Orders ID: #6735</Box>
                 <Box sx={{ backgroundColor: 'rgb(255,183,89)', padding: '5px', fontSize: '14px', fontWeight: '600', borderRadius: '6px' }}>Pending</Box>
@@ -17,6 +20,11 @@ function OrderShip() {
                     <CustomButton onclick={() => { }} sxprops={{ sx: { width: '100px', height: '48px' } }} variant="contained" text="Save" />
                 </Stack>
             </Stack>
+            <Box display={'flex'} gap={2} flexWrap={'wrap'}>
+                <OrderInfoCard icon={<PersonOutlinedIcon sx={{ fontSize: '35px' }} />} header={'Customer'} line1={'Full Name : Shisti@gmail.com'} line2={' Name : Shisti@gmail.com'} line3={'Phone : 9324292424'} />
+                <OrderInfoCard icon={<ShoppingBagOutlinedIcon sx={{ fontSize: '35px' }} />} header={'Order Info'} line1={'Full Name : Shisti@gmail.com'} line2={' Name : Shisti@gmail.com'} line3={'Status : Pending'} />
+                <OrderInfoCard icon={<ShoppingBagOutlinedIcon sx={{ fontSize: '35px' }} />} header={'Deliver to'} line1={'Full Name : Shisti@gmail.com'} line2={' Name : Shisti@gmail.com'} line3={'Status : Pending'} />
+            </Box>
         </Stack>
     )
 }
