2d1bc3fdcc7f71f1bce039433aec5f3027715a1d
Keshavsaini22 <keshavsainikesu@gmail.com>
2024-03-27 00:54:33 +0530

    wtf

---
+++ ztfrontend/src/pages/Dashboard/SideGraph/index.tsx
@@ -0,0 +1,33 @@
+import { Box, Divider, Stack } from '@mui/material'
+import React from 'react'
+import CustomButton from '../../../components/CustomButton'
+import { LineChart } from '@mui/x-charts/LineChart';
+
+function SideGraph() {
+    return (
+        <Stack p={2} sx={{ backgroundColor: 'white', borderRadius: '10px' }} gap={1}>
+            <Stack direction={'row'} justifyContent={'space-between'}>
+                <Box fontWeight={'600'}>Sale Graph</Box>
+                <Stack direction={'row'} gap={1}>
+                    <CustomButton onclick={() => { }} sxprops={{ sx: { width: '150px', height: '35px' } }} variant="outlined" text="Weekly" />
+                    <CustomButton onclick={() => { }} sxprops={{ sx: { width: '150px', height: '35px' } }} variant="contained" text="Monthly" />
+                    <CustomButton onclick={() => { }} sxprops={{ sx: { width: '150px', height: '35px' } }} variant="outlined" text="Yearly" />
+                </Stack>
+            </Stack>
+            <Divider />
+            {/* <LineChart
+                xAxis={[{ data: [1, 2, 3, 5, 8, 10] }]}
+                series={[
+                    {
+                        data: [2, 5.5, 2, 8.5, 1.5, 5],
+                        area: true,
+                    },
+                ]}
+                width={500}
+                height={300}
+            /> */}
+        </Stack>
+    )
+}
+
+export default SideGraph
